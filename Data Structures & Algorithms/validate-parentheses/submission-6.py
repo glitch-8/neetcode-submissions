75920e4fdbf63a406e6feaa4c8add3a8570ab18e
glitch-8 <127011512+glitch-8@users.noreply.github.com>
2026-09-23 14:09:40 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)<=1:
+            return False
+
+        hashmap = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+
+        stack = list()
+        for i in range(len(s)):
+            if s[i] in ['}', ']', ')']:
+                # pop the element from the stack
+                if not stack or hashmap[s[i]] != stack.pop():
+                    return False
+
+            else:
+                stack.append(s[i])
+        
+        return not stack
